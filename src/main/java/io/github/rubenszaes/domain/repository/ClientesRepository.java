7c57125db3ed7551adc7a8b8c3ca28cd5db74f83
Rubens Stanley Almeida Brandão <rubensalmeida@sefaz.pi.gov.br>
2022-09-29 08:53:10 -0300

    Delete por nome

--- src/main/java/io/github/rubenszaes/domain/repository/ClientesRepository.java
+++ src/main/java/io/github/rubenszaes/domain/repository/ClientesRepository.java
@@ -1,6 +1,7 @@
 package io.github.rubenszaes.domain.repository;
 import io.github.rubenszaes.domain.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -14,5 +15,10 @@ public interface ClientesRepository extends JpaRepository<Cliente, Integer> {
 
     @Query(value = " SELECT c FROM Cliente c WHERE c.nome LIKE :nome ")
     List<Cliente> encontrarPorNome(@Param("nome") String nome);
+
+    @Query( "DELETE FROM Cliente c WHERE c.nome =:nome ")
+    @Modifying
+    void deleteByNome(String nome);
+
     boolean existsByNome(String nome);
 }
